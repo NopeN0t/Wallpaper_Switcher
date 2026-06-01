59e91ade52ad8cfc75f9825395f9b0e415dd9f07
NopeN0t <top7748@gmail.com>
2026-06-01 17:48:26 +0700

    New UI Prototype

---
+++ Wallpaper Switcher/MainWindow.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia.Controls;
+
+namespace Wallpaper_Switcher_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+        }
+
+        private void ImageList_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void ShuffleButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+        }
+
+        private void ResetButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+        }
+    }
+}
